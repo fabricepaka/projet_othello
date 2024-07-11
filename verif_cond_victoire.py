9bdf5c179e5b61ded717f2375ce4d3fc08ff8f65
silvio-sjsj <silvio.dasilva@le-campus-numerique.fr>
2024-07-11 10:54:49 +0200

    updating branch with crrections and the wining condition

--- verif_cond_victoire.py
+++ verif_cond_victoire.py
@@ -1,29 +1,31 @@
 """."""
+from board import Board
 
 class Verifier_la_condition_de_victoire(Board):
 
-    def __init__(self, rows, columns):
-        Board.__init__(self, rows, columns)
+    def __init__(self, rows=6, columns=7):
+        Board.__init__(self, rows=6, columns=7)
     
     def verifier_cond(self, grid, pion):
 
         for row in range(self.rows):
             for col in range(self.columns):
-                if self.grid[row][col] == self.pion:
-
-                    col + 3 < self.columns and all(self.grid[col][col+1] == pion for i in rang(4)):
-                    return True
-
-
-
-
-
-
-
-
-
-
-    
-
-
-        raise NotImplementedError
+                if grid[row][col] == pion:
+                    
+                    # Checagem horizontal
+                    if col + 3 < self.columns and all(grid[row][col+i] == pion for i in range(4)):
+                        return True
+                    
+                    # checagem vertical
+                    elif row + 3 < self.rows and all(grid[row+i][col] == pion for i in range(4)):
+                        return True
+                    
+                    # checagem diagonal para a direita
+                    elif row >= 3 and col+3 < self.columns and all(grid[row-i][col+i] == pion for i in range(4)):
+                        return True
+                    
+                    # checagem diagonal para a esquerda
+                    elif row >= 3 and col >= 3 and all(grid[row-i][col-i] == pion for i in range(4)):
+                        return True
+        
+        return False
